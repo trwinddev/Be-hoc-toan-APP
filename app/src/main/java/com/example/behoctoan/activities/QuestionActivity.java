fa6406e1dfd5b9d3ffeb36c1d93e61cdd9dc1157
phongngotrong <ngotrongphong.dev@gmail.com>
2023-04-20 23:53:02 +0700

    Handle Question Activity

--- app/src/main/java/com/example/behoctoan/activities/QuestionActivity.java
+++ app/src/main/java/com/example/behoctoan/activities/QuestionActivity.java
@@ -1,17 +1,168 @@
 package com.example.behoctoan.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.behoctoan.Models.QuestionModel;
 import com.example.behoctoan.R;
+import com.example.behoctoan.databinding.ActivityQuestionBinding;
+
+import java.util.ArrayList;
 
 public class QuestionActivity extends AppCompatActivity {
-    
+
+    ArrayList<QuestionModel> list = new ArrayList<>();
+    private int count = 0;
+    private int position = 0;
+    private int score = 0;
+    CountDownTimer timer;
+
+    ActivityQuestionBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question);
+        binding = ActivityQuestionBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+        String setName = getIntent().getStringExtra("set");
+
+        if(setName.equals("BÀI LUYỆN TẬP 1")) {
+            setOne();
+        } else if (setName.equals("BÀI LUYỆN TẬP 2")) {
+            setTwo();
+        }
+        
+        for(int i=0; i<4; i++) {
+            binding.optionContainer.getChildAt(i).setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    checkAnswer((Button) view);
+                }
+            });
+        }
+
+        playAnimation(binding.question, 0, list.get(position).getQuestion());
+
+        binding.btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.btnNext.setEnabled(false);
+                binding.btnNext.setAlpha((float) 0.3);
+                enableOption(true);
+                position++;
+
+                if(position == list.size()) {
+                    Intent intent = new Intent(QuestionActivity.this, ScoreActivity.class);
+                    intent.putExtra("score", score);
+                    intent.putExtra("total", list.size());
+                    startActivity(intent);;
+                    finish();
+                    return;
+                }
+                
+                count = 0;
+                playAnimation(binding.question, 0, list.get(position).getQuestion());
+            }
+        });
     }
+
+    private void playAnimation(View view, int value, String data) {
+        view.animate().alpha(value).scaleX(value).scaleY(value).setDuration(500).setStartDelay(100)
+                .setInterpolator(new DecelerateInterpolator()).setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(@NonNull Animator animator) {
+                        if(value==0 && count < 4) {
+                            String option = "";
+
+                            if(count==0) {
+                                option = list.get(position).getOptionA();
+                            }else if(count==1) {
+                                option = list.get(position).getOptionB();
+                            }else if(count==2) {
+                                option = list.get(position).getOptionC();
+                            }else if(count==3) {
+                                option = list.get(position).getOptionD();
+                            }
+
+                            playAnimation(binding.optionContainer.getChildAt(count), 0, option);
+                            count++;
+                        }
+                    }
+
+                    @Override
+                    public void onAnimationEnd(@NonNull Animator animation) {
+                        if(value==0) {
+                            try {
+                                ((TextView)view).setText(data);
+                                binding.totalQuestion.setText(position+1+"/"+list.size());
+                            }catch (Exception e) {
+                                ((Button)view).setText(data);
+                            }
+
+                            view.setTag(data);
+                            playAnimation(view, 1, data);
+                        }
+                    }
+
+                    @Override
+                    public void onAnimationCancel(@NonNull Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(@NonNull Animator animation) {
+
+                    }
+                });
+    }
+
+    private void enableOption(boolean enable) {
+        for (int i = 0; i < 4; i++) {
+            binding.optionContainer.getChildAt(i).setEnabled(enable);
+
+            if(enable) {
+                binding.optionContainer.getChildAt(i).setBackgroundResource(R.drawable.btn_opt);
+            }
+        }
+    }
+
+    private void checkAnswer(Button selectedOption) {
+        binding.btnNext.setEnabled(true);
+        binding.btnNext.setAlpha(1);
+
+        if(selectedOption.getText().toString().equals(list.get(position).getCorrectAnswer())) {
+            score++;
+            selectedOption.setBackgroundResource(R.drawable.right_answ);
+        }
+        else {
+            selectedOption.setBackgroundResource(R.drawable.wrong_answ);
+            Button correctOption = (Button) binding.optionContainer.findViewWithTag(list.get(position).getCorrectAnswer());
+            correctOption.setBackgroundResource(R.drawable.right_answ);
+        }
+    }
+
+    private void setOne(){
+        list.add(new QuestionModel("Số bé nhất trong các số: 8 , 3 , 10 , 6 là:", "A. 3", "B. 8", "C. 6", "D. 10", "A. 3"));
+
+        list.add(new QuestionModel("Số lớn nhất trong các số: 8 , 3 , 10 , 6 là:", "A. 3", "B. 8", "C. 6", "D. 10", "C. 10"));
+    }
+
+    private void setTwo(){
+        list.add(new QuestionModel("Số bé nhất trong các số: 8 , 3 , 10 , 6 là:", "A. 3", "B. 8", "C. 6", "D. 10", "A. 3"));
+
+        list.add(new QuestionModel("Số lớn nhất trong các số: 8 , 3 , 10 , 6 là:", "A. 3", "B. 8", "C. 6", "D. 10", "C. 10"));
+
+    }
+
 }
