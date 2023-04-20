e87cd10c56b663d0a1ccb01474994f0b685f52f8
phongngotrong <ngotrongphong.dev@gmail.com>
2023-04-20 17:21:37 +0700

    Activity Sets

--- app/src/main/java/com/example/behoctoan/MainActivity.java
+++ app/src/main/java/com/example/behoctoan/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.behoctoan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.behoctoan.activities.SetsActivity;
 
 public class MainActivity extends AppCompatActivity {
+    CardView class_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        class_1 = findViewById(R.id.class_1);
+
+        class_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SetsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/behoctoan/Models/SetModel.java
@@ -0,0 +1,18 @@
+package com.example.behoctoan.Models;
+
+public class SetModel {
+
+    String setName;
+
+    public SetModel(String setName) {
+        this.setName = setName;
+    }
+
+    public String getSetName() {
+        return setName;
+    }
+
+    public void setSetName(String setName) {
+        this.setName = setName;
+    }
+}
