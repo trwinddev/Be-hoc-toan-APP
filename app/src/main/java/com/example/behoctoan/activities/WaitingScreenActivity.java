58a0efe6d8803f3269b152a6edc87d54337f2690
phongngotrong <ngotrongphong.dev@gmail.com>
2023-04-20 08:54:50 +0700

    UI Waiting Screen

---
+++ app/src/main/java/com/example/behoctoan/activities/WaitingScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.behoctoan.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.behoctoan.R;
+
+public class WaitingScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiting_screen);
+    }
+}
