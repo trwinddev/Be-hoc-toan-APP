7e4511ecebc2e08eef3cdca49bfa79883dd22e81
phongngotrong <ngotrongphong.dev@gmail.com>
2023-04-20 09:00:50 +0700

    UI Category

--- app/src/main/java/com/example/behoctoan/activities/WaitingScreenActivity.java
+++ app/src/main/java/com/example/behoctoan/activities/WaitingScreenActivity.java
@@ -2,8 +2,11 @@ package com.example.behoctoan.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.example.behoctoan.MainActivity;
 import com.example.behoctoan.R;
 
 public class WaitingScreenActivity extends AppCompatActivity {
@@ -12,5 +15,15 @@ public class WaitingScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiting_screen);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WaitingScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
     }
 }
