5925fb66b944c594b3b7da34cec1414030c7ef48
phongngotrong <ngotrongphong.dev@gmail.com>
2023-04-27 01:43:34 +0700

    Result

--- app/src/main/java/com/example/behoctoan/activities/ScoreActivity.java
+++ app/src/main/java/com/example/behoctoan/activities/ScoreActivity.java
@@ -2,15 +2,48 @@ package com.example.behoctoan.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.behoctoan.R;
+import com.example.behoctoan.databinding.ActivityScoreBinding;
 
 public class ScoreActivity extends AppCompatActivity {
+    ActivityScoreBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_score);
+        binding = ActivityScoreBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        int totalScore = getIntent().getIntExtra("total", 0);
+        int correctAnswer = getIntent().getIntExtra("score", 0);
+
+        int wrong = totalScore - correctAnswer;
+
+        binding.totalQuestions.setText(String.valueOf(totalScore));
+        binding.rightAnsw.setText(String.valueOf(correctAnswer));
+
+        binding.wrongAnsw.setText(String.valueOf(wrong));
+
+        binding.btnRetry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ScoreActivity.this, SetsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        binding.btnQuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
